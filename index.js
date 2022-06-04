c494fda44833294e95586baaad2ad63ef72956b0
Dieg0Ds <diegoslv91@gmail.com>
2022-06-04 01:09:00 -0300

    Inicio EJS express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const homeRouter = require("./routes/home");
+const cadastroRouter = require("./routes/cadastro")
+
+
+app.set('view engine', 'ejs');
+
+
+
+
+app.use(express.static('public'));
+app.use(homeRouter);
+app.use(cadastroRouter)
+
+app.listen(3000, () => console.log("loading..."));
