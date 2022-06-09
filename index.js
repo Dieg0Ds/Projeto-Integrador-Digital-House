fc6fd7cc7aba486ed67197952abf983d5591edc6
Dieg0Ds <diegoslv91@gmail.com>
2022-06-09 13:59:59 -0300

    .

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const app = express();
 const homeRouter = require("./routes/home");
-// const cadastroRouter = require("./routes/home");
 const minhaContaRouter = require("./routes/minhaConta");
-// const sobreRouter = require("./routes/sobre");
 const politicaRouter = require("./routes/politica");
 const trabalheConoscoRouter = require("./routes/trabalheConosco");
+// const sobreRouter = require("./routes/sobre");
+// const cadastroRouter = require("./routes/home");
 
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(express.static('public'));
+
 app.use(homeRouter);
 app.use(minhaContaRouter);
-// app.use(sobreRouter);
 app.use(politicaRouter);
 app.use(trabalheConoscoRouter);
+// app.use(sobreRouter);
 // app.use(cadastroRouter);
 
 
